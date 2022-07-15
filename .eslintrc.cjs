ba9e6d2de6e000137eba13564bf33d621f8c2801
Dabee-ee <movie2529@naver.com>
2022-07-15 15:47:59 +0900

    chore: eslint extends에서 airbnb-base 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'airbnb-base',
-    'prettier',
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier',
-  ],
+  extends: ['prettier', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
   env: {
     'vue/setup-compiler-macros': true,
   },

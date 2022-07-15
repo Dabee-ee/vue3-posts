4258546b7bbf43f5b7fee0d7846a333112ff9bfb
Dabee-ee <movie2529@naver.com>
2022-07-15 18:17:40 +0900

    feat: router 기능 이용한 버튼 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h2>Home View</h2>
+    <p>{{ $route.path }}</p>
+    <button class="btn btn-primary" @click="goAboutPage">About 페이지로 이동</button>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goAboutPage = () => {
+  router.push('/about');
+};
 </script>
-
-<style></style>

a44b79d78c5dabfb81b056da7ec30258c9193e05
Dabee-ee <movie2529@naver.com>
2022-07-17 23:37:55 +0900

    feat: env 환경변수 설정

--- src/api/index.js
+++ src/api/index.js
@@ -6,5 +6,3 @@ function create(baseURL, options) {
 }
 
 export const posts = create(`${import.meta.env.VITE_APP_API_URL}posts/`);
-// development : http://localhost:5000/posts/
-// production : http://localhost:5001/posts/

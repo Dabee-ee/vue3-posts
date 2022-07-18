b38cd97711664066e3b2a9da8a5aa66134120ea2
Dabee-ee <movie2529@naver.com>
2022-07-19 00:54:51 +0900

    feat: plugin 추가

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(person, { name: '홍길동' });
+app.use(router);
+app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,43 @@
   <div>
     <h2>Home View</h2>
     <p>{{ $route.path }}</p>
-    <button class="btn btn-primary" @click="goAboutPage">About 페이지로 이동</button>
+    <p>{{ $route.name }}</p>
+    <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+    <hr class="my-4" />
+    <AppGrid :items="items" v-slot="{ item }">
+      <AppCard>{{ item }}</AppCard>
+    </AppGrid>
+    <hr class="my-4" />
+    <h2>{{ $person.name }}</h2>
+    <button class="btn btn-primary" @click="person.say">click person</button>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+export default {
+  created() {
+    // console.log(this.$person.name);
+    // this.$person.say();
+  },
+};
+</script>
+
+<script setup>
+import { inject, ref } from 'vue';
 import { useRouter } from 'vue-router';
+
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
+
 const router = useRouter();
 const goAboutPage = () => {
   router.push('/about');
 };
+
+const items = ref(['사과', '딸기', '포도', '바나나']);
+
+const person = inject('person');
+console.log('person.name: ', person.name);
 </script>
+
+<style lang="scss" scoped></style>

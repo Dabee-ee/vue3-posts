3010af71262bfde6f59d4df924736c28c25669f5
Dabee-ee <movie2529@naver.com>
2022-07-18 22:13:29 +0900

    feat: 모달 만들기 & 텔레포트 사용

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">{{ title }}</h5>
-      <p class="card-text">
-        {{ content }}
-      </p>
-      <small class="text-muted">
-        {{ createdAt }}
-      </small>
-    </div>
-  </div>
+  <AppCard>
+    <h5 class="card-title">{{ title }}</h5>
+    <p class="card-text">
+      {{ content }}
+    </p>
+    <p class="text-muted">{{ createdAt }}</p>
+    <template #footer>
+      <div class="d-flex flex-row-reverse">
+        <button class="btn p-0" @click.stop="$emit('modal')">
+          <i class="bi bi-emoji-sunglasses"></i>
+        </button>
+      </div>
+    </template>
+  </AppCard>
 </template>
 
 <script setup>
+import AppCard from '@/components/AppCard.vue';
 defineProps({
   title: {
     type: String,
@@ -25,6 +29,7 @@ defineProps({
     type: [String, Date, Number],
   },
 });
+defineEmits(['modal']);
 </script>
 
 <style lang="scss" scoped></style>
